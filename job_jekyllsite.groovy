23ca6650df9eb2cae16c7f7a5b8fe3a757bf0806
Shaun Keenan <shaun_keenan@gap.com>
2016-05-26 10:41:38 -0700

    user/email for tag

--- job_jekyllsite.groovy
+++ job_jekyllsite.groovy
@@ -2,8 +2,11 @@ workflowJob('Jekyll Pipeline') {
   definition {
     cpsScm {
       scm {
-          git('https://github.com/skeenan947/skeenan.net')
-          scriptPath('Jenkinsfile')
+        git('https://github.com/skeenan947/skeenan.net') { node -> // is hudson.plugins.git.GitSCM
+            node / gitConfigName('skeenan.net Jenkins')
+            node / gitConfigEmail('ci@skeenan.net')
+        }
+        scriptPath('Jenkinsfile')
       }
     }
   }
